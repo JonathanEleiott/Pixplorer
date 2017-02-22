0f96d967e153d231e4dd642ffbe1b6dcc1c8b1c7
dsvorc41 <danilosvorcan@live.com>
2017-02-22 12:06:12 -0800

    Replace google-vision with parent package google-cloud

--- server/test/test-server.js
+++ server/test/test-server.js
@@ -10,223 +10,223 @@ var generateParams = function(method, endpoint, optionalParams){
   optionalParams = optionalParams || '';
   return {
     method: method,
-      uri: 'http://198.199.94.223:8080/' + endpoint,
+      uri: 'http://localhost:8080/' + endpoint,
       form: optionalParams
   };
   //live server url: http://198.199.94.223:8080/
   //local server url: http://localhost:8080/
 };
 
-// describe('REST', function() {
-//   afterEach(function() {
-//     // logout user - runs after each test in this block
-//     var logoutParams = generateParams('POST', 'logout');
-//     request(logoutParams);
-//   });
+describe('REST', function() {
+  afterEach(function() {
+    // logout user - runs after each test in this block
+    var logoutParams = generateParams('POST', 'logout');
+    request(logoutParams);
+  });
 
-//   it('should list respond with status 200 on basic GET request', function(done) {
-//     var params = generateParams('GET', '');
-//     request(params, function(error, response, body) {
-//       expect(response.statusCode).to.equal(200);
-//       expect(body).to.equal('Welcome the server for Crustaceans thesis project!');
-//       done();
-//     });
-//   });
+  it('should list respond with status 200 on basic GET request', function(done) {
+    var params = generateParams('GET', '');
+    request(params, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome the server for Crustaceans thesis project!');
+      done();
+    });
+  });
 
-//   it('should list respond with status 404 for non-existend address', function(done) {
-//     var params = generateParams('GET', 'randomEndPoint');
-//     request(params, function(error, response, body) {
-//       expect(response.statusCode).to.equal(404);
-//       done();
-//     });
-//   });
-// });
+  it('should list respond with status 404 for non-existend address', function(done) {
+    var params = generateParams('GET', 'randomEndPoint');
+    request(params, function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
 
-// describe('AUTH', function() {
-//   afterEach(function() {
-//     // logout user - runs after each test in this block
-//     var logoutParams = generateParams('POST', 'logout');
-//     request(logoutParams);
-//   });
+describe('AUTH', function() {
+  afterEach(function() {
+    // logout user - runs after each test in this block
+    var logoutParams = generateParams('POST', 'logout');
+    request(logoutParams);
+  });
 
-//   it('should create a new user', function(done) {
-//     var createUserParams = generateParams('POST', 'createUser', {email: 'john2@aol.com', password: 'John123'});
+  it('should create a new user', function(done) {
+    var createUserParams = generateParams('POST', 'createUser', {email: 'john2@aol.com', password: 'John123'});
 
-//     request(createUserParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(201);
-//       expect(parsedBody.uid).to.exist;
-//       expect(parsedBody.email).to.equal('john2@aol.com');
-//       done();
-//     });
-//   });
+    request(createUserParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(201);
+      expect(parsedBody.uid).to.exist;
+      expect(parsedBody.email).to.equal('john2@aol.com');
+      done();
+    });
+  });
 
-//   it('should login existing user', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
-//     var logoutParams = generateParams('POST', 'logout');
+  it('should login existing user', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
+    var logoutParams = generateParams('POST', 'logout');
 
-//     request(loginParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(201);
-//       expect(parsedBody.email).to.equal('john@aol.com');
-//       done();
-//     });
-//   });
+    request(loginParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(201);
+      expect(parsedBody.email).to.equal('john@aol.com');
+      done();
+    });
+  });
 
-//   it('should login existing user using URLencoded content type (default test type)', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
-//     var logoutParams = generateParams('POST', 'logout');
+  it('should login existing user using URLencoded content type (default test type)', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
+    var logoutParams = generateParams('POST', 'logout');
 
-//     request(loginParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(201);
-//       expect(parsedBody.email).to.equal('john@aol.com');
-//       done();
-//     });
-//   });
+    request(loginParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(201);
+      expect(parsedBody.email).to.equal('john@aol.com');
+      done();
+    });
+  });
 
-//   it('should login existing user using JSON content type (instead of URLencoded)', function(done) {
-//     axios({
-//          method: 'post',
-//          url: 'http://localhost:8080/login',
-//          data: {email: 'john@aol.com', password: 'John123'}
-//        })
-//        .then(function(response) {
-//          expect(response.status).to.equal(201);
-//          expect(response.data.email).to.equal('john@aol.com');
-//          done();
-//        })
-//        .catch(function(error) {
-//          expect(error.status).to.equal(201);
-//          expect(error.data.email).to.equal('john@aol.com');
-//          done();
-//        });
-//   });
+  it('should login existing user using JSON content type (instead of URLencoded)', function(done) {
+    axios({
+         method: 'post',
+         url: 'http://localhost:8080/login',
+         data: {email: 'john@aol.com', password: 'John123'}
+       })
+       .then(function(response) {
+         expect(response.status).to.equal(201);
+         expect(response.data.email).to.equal('john@aol.com');
+         done();
+       })
+       .catch(function(error) {
+         expect(error.status).to.equal(201);
+         expect(error.data.email).to.equal('john@aol.com');
+         done();
+       });
+  });
 
-//   it('should logout a previously logged in user', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
-//     var logoutParams = generateParams('POST', 'logout');
+  it('should logout a previously logged in user', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
+    var logoutParams = generateParams('POST', 'logout');
 
-//     //first login
-//     request(loginParams, function(error, response, body) {
-//       //then logout to test
-//       request(logoutParams, function(error, response, body) {
-//         expect(response.statusCode).to.equal(201);
-//         expect(body).to.equal('Sign-out successful!');
-//         done();
-//       });
-//     });
-//   });
+    //first login
+    request(loginParams, function(error, response, body) {
+      //then logout to test
+      request(logoutParams, function(error, response, body) {
+        expect(response.statusCode).to.equal(201);
+        expect(body).to.equal('Sign-out successful!');
+        done();
+      });
+    });
+  });
 
-//   it('should not login user without existing account', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'nonexistentUser@aol.com', password: 'nonexistentUserPass'});
+  it('should not login user without existing account', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'nonexistentUser@aol.com', password: 'nonexistentUserPass'});
 
-//     request(loginParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(401);
-//       expect(parsedBody.message).to.contain('There is no user record corresponding to this identifier');
-//       done();
-//     });
-//   });
+    request(loginParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(401);
+      expect(parsedBody.message).to.contain('There is no user record corresponding to this identifier');
+      done();
+    });
+  });
 
-//   it('should not have a session without logging in', function(done) {
-//     var checkCredentialsParams = generateParams('GET', 'checkUserCredentials');
-//     request(checkCredentialsParams, function(error, response, body) {
-//       expect(response.statusCode).to.equal(401);
-//       expect(body).to.equal('User is not logged in!');
-//       done();
-//     });
-//   });
+  it('should not have a session without logging in', function(done) {
+    var checkCredentialsParams = generateParams('GET', 'checkUserCredentials');
+    request(checkCredentialsParams, function(error, response, body) {
+      expect(response.statusCode).to.equal(401);
+      expect(body).to.equal('User is not logged in!');
+      done();
+    });
+  });
 
-//   it('should have a session after logging in', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
-//     var checkCredentialsParams = generateParams('GET', 'checkUserCredentials');
+  it('should have a session after logging in', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john@aol.com', password: 'John123'});
+    var checkCredentialsParams = generateParams('GET', 'checkUserCredentials');
 
-//     //login
-//     request(loginParams, function(error, response, body) {
-//       //then check credentials
-//       request(checkCredentialsParams, function(error, response, body) {
-//         var parsedBody = JSON.parse(body);
-//         expect(response.statusCode).to.equal(200);
-//         expect(parsedBody.uid).to.exist;
-//         expect(parsedBody.stsTokenManager).to.exist;
-//         expect(parsedBody.stsTokenManager.accessToken).to.exist;
-//         expect(parsedBody.stsTokenManager.expirationTime).to.exist;
-//         expect(parsedBody.redirectEventId).to.be.null;
-//         done();
-//       });
-//     });
-//   });
+    //login
+    request(loginParams, function(error, response, body) {
+      //then check credentials
+      request(checkCredentialsParams, function(error, response, body) {
+        var parsedBody = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        expect(parsedBody.uid).to.exist;
+        expect(parsedBody.stsTokenManager).to.exist;
+        expect(parsedBody.stsTokenManager.accessToken).to.exist;
+        expect(parsedBody.stsTokenManager.expirationTime).to.exist;
+        expect(parsedBody.redirectEventId).to.be.null;
+        done();
+      });
+    });
+  });
 
-//   it('should delete an existing, logged in user', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john2@aol.com', password: 'John123'});
-//     var deleteUserParams = generateParams('POST', 'deleteUser');
+  it('should delete an existing, logged in user', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john2@aol.com', password: 'John123'});
+    var deleteUserParams = generateParams('POST', 'deleteUser');
 
-//     request(loginParams, function(error, response, body) {
-//       request(deleteUserParams, function(error, response, body) {
-//         expect(response.statusCode).to.equal(201);
-//         expect(body).to.equal('User deleted!');
-//         done();
-//       });
-//     });
-//   });
+    request(loginParams, function(error, response, body) {
+      request(deleteUserParams, function(error, response, body) {
+        expect(response.statusCode).to.equal(201);
+        expect(body).to.equal('User deleted!');
+        done();
+      });
+    });
+  });
 
-//   it('should NOT delete a user if they are not logged in', function(done) {
-//     var deleteUserParams = generateParams('POST', 'deleteUser');
-//     //attempt to delete without logging in first
-//     request(deleteUserParams, function(error, response, body) {
-//       expect(response.statusCode).to.equal(401);
-//       expect(body).to.equal('User not logged in, or doesn\'t exist!');
-//       done();
-//     });
-//   });
+  it('should NOT delete a user if they are not logged in', function(done) {
+    var deleteUserParams = generateParams('POST', 'deleteUser');
+    //attempt to delete without logging in first
+    request(deleteUserParams, function(error, response, body) {
+      expect(response.statusCode).to.equal(401);
+      expect(body).to.equal('User not logged in, or doesn\'t exist!');
+      done();
+    });
+  });
 
-//   it('should NOT delete a user if they don\'t have an account', function(done) {
-//     var loginParams = generateParams('POST', 'login', {email: 'john2123@aol.com', password: 'John123'});
-//     var deleteUserParams = generateParams('POST', 'deleteUser');
+  it('should NOT delete a user if they don\'t have an account', function(done) {
+    var loginParams = generateParams('POST', 'login', {email: 'john2123@aol.com', password: 'John123'});
+    var deleteUserParams = generateParams('POST', 'deleteUser');
 
-//     request(loginParams, function(error, response, body) {
-//       request(deleteUserParams, function(error, response, body) {
-//         expect(response.statusCode).to.equal(401);
-//         expect(body).to.equal('User not logged in, or doesn\'t exist!');
-//         done();
-//       });
-//     });
-//   });
+    request(loginParams, function(error, response, body) {
+      request(deleteUserParams, function(error, response, body) {
+        expect(response.statusCode).to.equal(401);
+        expect(body).to.equal('User not logged in, or doesn\'t exist!');
+        done();
+      });
+    });
+  });
 
-//   it('should NOT create a new user if the account already exists', function(done) {
-//     var createUserParams = generateParams('POST', 'createUser', {email: 'john@aol.com', password: 'John123'});
-//     request(createUserParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(400);
-//       expect(body).to.contain('The email address is already in use by another account');
-//       done();
-//     });
-//   });
+  it('should NOT create a new user if the account already exists', function(done) {
+    var createUserParams = generateParams('POST', 'createUser', {email: 'john@aol.com', password: 'John123'});
+    request(createUserParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(400);
+      expect(body).to.contain('The email address is already in use by another account');
+      done();
+    });
+  });
 
-//   it('should NOT create a new user with invalid email format', function(done) {
-//     var createUserParams = generateParams('POST', 'createUser', {email: 'johnaol.com', password: 'John123'});
+  it('should NOT create a new user with invalid email format', function(done) {
+    var createUserParams = generateParams('POST', 'createUser', {email: 'johnaol.com', password: 'John123'});
 
-//     request(createUserParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(400);
-//       expect(body).to.contain('The email address is badly formatted');
-//       done();
-//     });
-//   });
+    request(createUserParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(400);
+      expect(body).to.contain('The email address is badly formatted');
+      done();
+    });
+  });
 
-//   it('should NOT create a new user with password that is less than 6 characters long', function(done) {
-//     var createUserParams = generateParams('POST', 'createUser', {email: 'john2@aol.com', password: '1234'});
+  it('should NOT create a new user with password that is less than 6 characters long', function(done) {
+    var createUserParams = generateParams('POST', 'createUser', {email: 'john2@aol.com', password: '1234'});
     
-//     request(createUserParams, function(error, response, body) {
-//       var parsedBody = JSON.parse(body);
-//       expect(response.statusCode).to.equal(400);
-//       expect(body).to.contain('Password should be at least 6 characters');
-//       done();
-//     });
-//   });
+    request(createUserParams, function(error, response, body) {
+      var parsedBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(400);
+      expect(body).to.contain('Password should be at least 6 characters');
+      done();
+    });
+  });
 
-// });
+});
 
 describe('IMAGE UPLOAD', function() {
 
