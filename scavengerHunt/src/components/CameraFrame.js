d9e6d4d2898f3a6a9af00349943e426abde97dd0
Bill Lea <bill.lea@gmail.com>
2017-02-24 12:25:02 -0800

    Progress: splash screen added

--- scavengerHunt/src/components/CameraFrame.js
+++ scavengerHunt/src/components/CameraFrame.js
@@ -3,15 +3,28 @@ import {
   Dimensions,
   StyleSheet,
   Text,
-  Vibration,
-  ImageStore
+  View, 
+  Vibration
 } from 'react-native';
 import Camera from 'react-native-camera';
 import axios from 'axios';
-import RNFetchBlob from 'react-native-fetch-blob'
+import RNFetchBlob from 'react-native-fetch-blob';
+import { Card, CardSection, Button, Input } from './mostCommon';
 
 class CameraFrame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 1
+    };
+    console.log('CameraFrame Props:', props);
+  }
 
+  openCamera() {
+    this.setState({
+      status: 2
+    });
+  }
 
   takePicture() {
     console.log('PRESSED');
@@ -23,26 +36,24 @@ class CameraFrame extends Component {
         RNFetchBlob.fs.readFile(data.path, 'base64')
         .then((imageData) => {
           // handle the data ..
-          console.log(imageData.length);
+          console.log('Image Size:', imageData.length);
           axios({
               method: 'post',
               responseType: 'arraybuffer',
               url: 'http://198.199.94.223:8080/postImage',
-              data: {imageBuffer: imageData}
+              data: { imageBuffer: imageData }
             })
-            .then(function(response) {
-              expect(response.status).to.equal(201);
+            .then((response) => {
+              console.log('Image sent to server:', response.data);
             })
-            .catch(function(error) {
-              console.log('error');
+            .catch((error) => {
+              console.log('Error sending image to server', error);
             });
-          })
-        })
-
-
+          });
+        });
   }
 
-  render() {
+  renderCamera() {
     return (
       <Camera
         ref={(cam) => {
@@ -56,6 +67,37 @@ class CameraFrame extends Component {
         <Text style={styles.capture} onPress={this.takePicture.bind(this)}>SNAP THE PIC!!</Text>
       </Camera>
     );
+  }
+
+  renderSplash() {
+    return (
+      <View style={styles.splash}>
+      <Text style={styles.splashHeader}>Add an Item</Text>
+        <Text style={styles.splashText}>
+          Step 1: Take a Photo
+        </Text>
+        <Text style={styles.splashInstructions}>
+          Your first step is to take a photo, then you will give it a name.
+        </Text>
+        <Text style={styles.capture} onPress={this.openCamera.bind(this)}>OK, I GOT IT!</Text>
+      </View>
+    );
+  }
+
+  render() {
+    if (this.state.status === 1) {
+      return (
+        <View style={styles.container}>
+        {this.renderSplash()}
+        </View>
+      );
+    } else if (this.state.status === 2) {
+      return (
+        <View style={styles.container}>
+        {this.renderCamera()}
+        </View>
+      );
+    }
   }
 }
 
@@ -84,6 +126,39 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 20,
     marginBottom: 400
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }, 
+  splash: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: '#4b7ccc'
+  },
+  splashHeader: {
+    flex: 0,
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 28,
+    padding: 10,
+    margin: 20,
+    marginBottom: 200
+  },
+  splashText: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 0,
+  },
+  splashInstructions: {
+    textAlign: 'center',
+    color: '#fff',
+    marginBottom: 5,
   },
 
 });
