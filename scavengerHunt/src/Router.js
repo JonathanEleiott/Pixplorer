63a8810dd2858f0d6f0a7608d725cfc8eaddc319
billylea <bill.lea@gmail.com>
2017-02-25 10:33:04 -0800

    Update Router.js

--- scavengerHunt/src/Router.js
+++ scavengerHunt/src/Router.js
@@ -12,9 +12,6 @@ import CameraFrame from './components/CameraFrame';
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="auth">
-        <Scene key="login" component={LoginForm} title="Please Login" />
-      </Scene>
       <Scene key="main">
         <Scene
           key="listChooser"
