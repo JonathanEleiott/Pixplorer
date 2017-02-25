fe14cb3d4e1fd8c0cacfadb9d416dfbc9366633a
dsvorc41 <danilosvorcan@live.com>
2017-02-24 20:23:14 -0800

    Implement GoogleVision and MongoDB on a separate service

--- server/scripts/requestHandler.js
+++ server/scripts/requestHandler.js
@@ -1,5 +1,6 @@
 const gcloud = require('google-cloud');
 const AWS = require('aws-sdk');
+const axios = require('axios');
 const firebase = require('./firebaseConfig');
 const headers = require('./headers');
 
@@ -92,26 +93,25 @@ module.exports = {
   },
   
   postImage: (req, res) => {
+    //http://localhost:8084/imageMockRoute
     console.log(`Serving ${req.method} request for ${req.url} (inside requestHandler.postImage)`);
     const randomImageName = `${Math.random()}.jpg`;
-    const imageBuffer = new Buffer(req.body.imageBuffer, 'base64');
+    const imageData = new Buffer(req.body.imageBuffer, 'base64');
 
-    const params = {
-      Bucket: 'image-upload-folder',
-      Key: randomImageName,
-      Body: imageBuffer
-    };
-
-    s3.upload(params, (err, data) => {
-      if (err) {
-        console.log('Upload Error ', err);
+    axios({
+        method: 'post',
+        responseType: 'arraybuffer',
+        url: 'http://localhost:8084/setImage',
+        data: { imageBuffer: imageData }
+      })
+      .then((response) => {
+        console.log('image successfuly posted');
+        sendResponse(res, 201, headers, 'Image successfuly saved!');
+      })
+      .catch((error) => {
+        console.log('AXIOS ERROR', error);
         sendResponse(res, 404, '', 'Error');
-      } if (data) {
-        console.log('Upload Success ', data.Location);
-        //sendResponse(res, 201, headers, 'Image successfuly saved!');
-        res.json(data);
-      }
-    });   
+      });  
   },
 
   gVision: (req, res) => {
