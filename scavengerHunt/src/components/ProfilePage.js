44f4f579e53fd8d968339b72c2b475bb2239fbce
dsvorc41 <danilosvorcan@live.com>
2017-03-03 20:06:41 -0800

    Implement front facing camera on profile page (update pic)

--- scavengerHunt/src/components/ProfilePage.js
+++ scavengerHunt/src/components/ProfilePage.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
-import { connect } from 'react-redux';
-// import { Actions } from 'react-native-router-flux';
-import { Card, CardSection, Button } from './mostCommon';
-import { logoutUser, updateProfile } from '../actions';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Vibration,
+  Image
+} from 'react-native';
+import Camera from 'react-native-camera';
+import axios from 'axios';
+import RNFetchBlob from 'react-native-fetch-blob';
 
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+
+import { Spinner, Card, CardSection, Input, Button } from './mostCommon';
+import { logoutUser } from '../actions';
 
 class ProfilePage extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      currentScreen: 'profilePage'
+    };
   }
+  
 
   componentWillMount() {
     //ensure user is logged in
@@ -18,90 +33,123 @@ class ProfilePage extends Component {
     if (!userLoggedIn) {
       //Actions.auth();
     }
+  }
 
-    this.props.updateProfile(this.props.userID);
+  takePicture() {
+    console.log('PRESSED');
+    this.camera.capture()
+      .then((data) => {
+        Vibration.vibrate();
+        console.log('DATA IMG:', data.path);
+
+        this.setState({
+          currentScreen: 'profilePage'
+        });
+
+        RNFetchBlob.fs.readFile(data.path, 'base64')
+        .then((imageData) => {
+          // axios({
+          //     method: 'post',
+          //     url: `${config.mainServer}/postImage`,
+          //     data: { 
+          //       imageBuffer: imageData,
+          //       targetImageLatitude: this.state.targetImageLatitude,
+          //       targetImageLongitude: this.state.targetImageLongitude,
+          //       targetImageAllowedDistance: 30 //currently hardcoded
+          //     }
+          //   })
+          //   .then((response) => {
+          //     console.log('SUCCESS: Image sent to server:', response.data);
+          //     this.setState({
+          //       status: 4,
+          //       newItemURL: response.data
+          //     });
+          //   })
+          //   .catch((error) => {
+          //     console.log('Error sending image to server', error);
+          //   });
+          });
+        });
+  }
+
+  renderProfilePage() {
+    return (
+      <Card>
+        <CardSection>
+          <Image
+           style={styles.profilePhotoStyle}
+           source={{ uri: 'https://cdn.pixabay.com/photo/2016/05/23/23/32/human-1411499_1280.jpg' }}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Text style={styles.textStyle}>
+           Email Address:
+          </Text>
+          <Text style={styles.textStyle}>
+           gandalfTheGrey@gmail.com
+          </Text>
+        </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => { 
+              console.log('pressed logout!'); 
+              this.setState({
+                currentScreen: 'frontCamera'
+              });
+            }}
+          >
+             Update Profile Photo
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+             Change Password
+          </Button>
+        </CardSection>
+
+        <CardSection>
+         <Button 
+           onPress={() => { 
+             console.log('pressed logout!'); 
+             this.props.logoutUser();
+           }}
+         >
+            Log out
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+
+  renderFrontCamera() {
+    return (
+      <View style={styles.container}>       
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          type="front"
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          captureTarget={Camera.constants.CaptureTarget.cameraRoll}
+          defaultTouchToFocus
+        >
+          <Text style={styles.header}>List Item Found...</Text>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>SNAP THE PIC!!</Text>
+        </Camera>
+      </View>
+    );
   }
 
   render() {
-   return (
-     <Card>
-       <CardSection>
-         <Image
-          style={styles.profilePhotoStyle}
-          source={{ uri: 'https://cdn.pixabay.com/photo/2016/05/23/23/32/human-1411499_1280.jpg' }}
-         />
-       </CardSection>
-
-       <CardSection>
-         <Text style={styles.textStyle}>
-          Email Address:
-         </Text>
-         <Text style={styles.textStyle}>
-          gandalfTheGrey@gmail.com
-         </Text>
-       </CardSection>
-
-       <CardSection>
-         <Text style={styles.textStyle}>
-          Number of Created Lists
-         </Text>
-         <Text style={styles.textStyle}>
-          { this.props.userStats }
-         </Text>
-       </CardSection>
-
-       <CardSection>
-         <Text style={styles.textStyle}>
-          Number of Subscribed Lists
-         </Text>
-         <Text style={styles.textStyle}>
-          { this.props.userStats }
-         </Text>
-       </CardSection>
-
-       <CardSection>
-         <Text style={styles.textStyle}>
-          Number of Items checked off
-         </Text>
-         <Text style={styles.textStyle}>
-          { this.props.userStats }
-         </Text>
-       </CardSection>
-
-       <CardSection>
-         <Text style={styles.textStyle}>
-          Number of Bunnies Found
-         </Text>
-         <Text style={styles.textStyle}>
-          322
-         </Text>
-       </CardSection>
-
-       <CardSection>
-         <Button>
-            Update Profile Photo
-         </Button>
-       </CardSection>
-
-       <CardSection>
-         <Button>
-            Change Password
-         </Button>
-       </CardSection>
-
-       <CardSection>
-        <Button
-          onPress={() => {
-            console.log('pressed logout!');
-            this.props.logoutUser();
-          }}
-        >
-           Log out
-         </Button>
-       </CardSection>
-
-     </Card>
-   );
+    if (this.state.currentScreen === 'profilePage') {
+      return this.renderProfilePage();
+    } else if (this.state.currentScreen === 'frontCamera') {
+      return this.renderFrontCamera();
+    }
   }
 }
 
@@ -119,6 +167,37 @@ const styles = {
     height: 260,
     flex: 1,
     borderRadius: 60
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40,
+    fontSize: 20,
+  },
+  header: {
+    flex: 0,
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 28,
+    padding: 10,
+    margin: 20,
+    marginBottom: 400
   }
 };
 
