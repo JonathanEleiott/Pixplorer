0f8caf6372d6eaac386a0c5b707f42d8f1ddb46d
Jonathan <jonathan.eleiott@gmail.com>
2017-03-06 19:22:52 -0800

    Pie Chart

--- scavengerHunt/src/components/ListStats.js
+++ scavengerHunt/src/components/ListStats.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
+import { Pie } from 'react-native-pathjs-charts';
 import { Card, CardSection } from './mostCommon';
 
 class ListStats extends Component {
 
   render() {
     const { list } = this.props;
+
+    const data = [{
+      name: 'Washington',
+      population: 7694980
+    }, {
+      name: 'Oregon',
+      population: 2584160
+    }, {
+      name: 'Minnesota',
+      population: 6590667
+    }, {
+      name: 'Alaska',
+      population: 7284698
+    }];
+
+    const options = {
+      margin: {
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+      },
+      width: 350,
+      height: 350,
+      color: '#2980B9',
+      r: 50,
+      R: 150,
+      legendPosition: 'topLeft',
+      animate: {
+        type: 'oneByOne',
+        duration: 200,
+        fillTransition: 3
+      },
+      label: {
+        fontFamily: 'Arial',
+        fontSize: 8,
+        fontWeight: true,
+        color: '#ECF0F1'
+      }
+    };
 
    return (
      <Card>
@@ -21,15 +62,21 @@ class ListStats extends Component {
           Total Items in List - { list.items.length }
          </Text>
        </CardSection>
+       <Pie
+          data={data}
+          options={options}
+          accessorKey="population"
+       />
      </Card>
    );
   }
 }
 
-const mapStateToProps = ({ core }) => {
+const mapStateToProps = ({ core, auth }) => {
   const { list, timeStamps } = core;
+  const { userID } = auth;
 
-  return { list };
+  return { list, userID };
 };
 
 export default connect(mapStateToProps, {})(ListStats);
